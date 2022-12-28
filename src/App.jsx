06c8b6173fe1bb3461b8e7393c6d8c87f87c0470
HerlanBenavides <herlanbenavidesp@gmail.com>
2022-12-28 00:08:04 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import './App.css'
+import Loading from './components/Loading'
 import WeatherCard from './components/WeatherCard'
 
 function App() {
@@ -11,46 +12,49 @@ function App() {
   const [temperature, setTemperature] = useState()
   const [iscelsius, setIscelsius] = useState(true)
 
-  useEffect(()=>{
+  useEffect(() => {
     const success = (pos) => {
-      const obj={
-        lat:pos.coords.latitude,
-        lon:pos.coords.longitude
-      }     
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+      }
       setCoords(obj);
     }
     //Esto hace el llamdo a la api del navegador, para usar la ubicacion actual
     navigator.geolocation.getCurrentPosition(success)
-  },[])
+  }, [])
 
   //console.log(coords)
 
   //----------Peticion del Clima--------
 
-useEffect(()=>{
-  if (coords){
-    const APIKEY = '1f883653ebbc11a3ba0c45670515dd87'
-    const URL =`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKEY}`
-    axios.get(URL)
-    .then(res =>{ 
-      const celsius = (res.data.main.temp -273.15).toFixed(1)
-      const farenheit = ((celsius*9/5)+35).toFixed(1)
-      setTemperature({celsius, farenheit})
-      setWeather(res.data)
+  useEffect(() => {
+    if (coords) {
+      const APIKEY = '1f883653ebbc11a3ba0c45670515dd87'
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKEY}`
+      axios.get(URL)
+        .then(res => {
+          const celsius = (res.data.main.temp - 273.15).toFixed(1)
+          const farenheit = ((celsius * 9 / 5) + 35).toFixed(1)
+          setTemperature({ celsius, farenheit })
+          setWeather(res.data)
 
-    })
-    .catch(err=> console.log(err))
+        })
+        .catch(err => console.log(err))
 
-  }
-  
-},[coords])
+    }
 
-console.log(weather)
+  }, [coords])
+
+  console.log(weather)
 
   return (
     <div className="App">
-      <WeatherCard weather={weather} temperature={temperature}/>
-
+      {weather ?
+        <WeatherCard weather={weather} temperature={temperature} />
+        :
+        <Loading />
+      }
     </div>
   )
 }
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const WeatherCard = ({weather, temperature}) => {
+const WeatherCard = ({ weather, temperature }) => {
 
   const [iscelsius, setIscelsius] = useState(true)
 
@@ -8,25 +8,31 @@ const WeatherCard = ({weather, temperature}) => {
 
   console.log(weather)
   return (
-    <article>
-        <h1>Weather App</h1>
-        <h2>{`${weather?.name}, ${weather?.sys.country}`}</h2>
-        <section>
-          <img src={weather && `http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="" />
-          
-        </section>
+    <article className="inicio">
+      <h1>Weather App</h1>
+      <h2>{`${weather?.name}, ${weather?.sys.country}`}</h2>
+      <section>
+        <img src={weather && `http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="" />
 
-        <section>
-          <h3>"{weather?.weather[0].description}"</h3>
-          <ul>
-            <li><span>Wind Speed</span> {weather?.wind.speed} m/s</li>
-            <li><span>Clouds</span> {weather?.clouds.all} % </li>
-            <li><span>Pressure</span> {weather?.main.pressure} hPa </li>
-          </ul>
-        </section>
+      </section>
 
-        <h2>{iscelsius ? `${temperature?.celsius} C` : `${temperature?.farenheit} F`}</h2>
-        <button onClick={changeTemperature}>{iscelsius ? 'Change F' : 'Chance to C'}</button>
+      <section>
+        <h3>"{weather?.weather[0].description}"</h3>
+        <ul>
+          <li><span>Wind Speed</span> {weather?.wind.speed} m/s</li>
+          <li><span>Clouds</span> {weather?.clouds.all} % </li>
+          <li><span>Pressure</span> {weather?.main.pressure} hPa </li>
+        </ul>
+      </section>
+
+      <h2>{iscelsius ? `${temperature?.celsius} C` : `${temperature?.farenheit} F`}</h2>
+      <button onClick={changeTemperature}>{iscelsius ? 'Change F' : 'Chance to C'}</button>
+
+      <footer className='uno'>Realizado por:</footer>
+      <footer>Herlan Benavides</footer>
+      <footer>Email: </footer>
+      <footer>herlanbenavidesp@gmail.com</footer>
+
 
     </article>
   )
