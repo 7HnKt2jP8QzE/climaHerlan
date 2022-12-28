d8b3734673584a44e130479ae35041d8d5ae8562
HerlanBenavides <herlanbenavidesp@gmail.com>
2022-12-27 19:01:00 -0600

    Update WeatherCard.jsx

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,7 +6,20 @@ const WeatherCard = ({weather}) => {
     <article>
         <h1>Weather App</h1>
         <h2>{`${weather?.name}, ${weather?.sys.country}`}</h2>
-        
+        <section>
+          <img src={weather && `http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="" />
+          <h3>Temperatura</h3>
+        </section>
+
+        <section>
+          <h3>"{weather?.weather[0].description}"</h3>
+          <ul>
+            <li><span>Wind Speed</span> {weather?.wind.speed} m/s</li>
+            <li><span>Clouds</span> {weather?.clouds.all} % </li>
+            <li><span>Pressure</span> {weather?.main.pressure} hPa </li>
+          </ul>
+        </section>
+
     </article>
   )
 }
